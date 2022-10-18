3e9c097ffc6e93ac13fd66d5509851c12e3b49d5
dimakhodosevich <dimakhodosevich95@gmail.com>
2022-10-18 22:22:07 +0300

    Add sout(s) for checking debug logic.

--- src/by/khodosevich/log4gpractice/module/Factorial.java
+++ src/by/khodosevich/log4gpractice/module/Factorial.java
@@ -4,11 +4,14 @@ import by.khodosevich.log4gpractice.module.exception.WrongNumberException;
 
 public class Factorial {
     public static int factorial(int number) throws WrongNumberException{
+        System.out.println("Method starts with arg = " + number);
         if(number<0){
+            System.out.println("Exception. Wrong number!!! Number should be positive.");
             throw new WrongNumberException("Your number less then 0");
         }
 
         int factorial = 1;
+        System.out.println("Algorithm starts with factorial = " + factorial);
 
         if(number==0 || number == 1){
             return factorial;
@@ -18,6 +21,7 @@ public class Factorial {
             factorial *= i;
         }
 
+        System.out.println("Method ends with result = "+ factorial);
         return  factorial;
     }
 }
